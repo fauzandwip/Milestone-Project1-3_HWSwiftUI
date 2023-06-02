1c6800a1f85624e6140ad924d6529cc3c227cea7
fauzandp <fauzan27dp@gmail.com>
2023-06-02 21:03:00 +0700

    Milestone-Project1-3_HWSwiftUI | RockPapperScissors Game add background and change font style

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -19,76 +19,81 @@ struct ContentView: View {
     @State private var winOrLose = ""
     @State private var isHidden = true
     
-    private let maxPlay = 3
+    private let maxPlay = 10
     @State private var score = 0
     @State private var userPlayed = 0
     
     var body: some View {
         NavigationStack {
-            VStack {
-                HStack {
-                    Text("Score: \(score)")
-                        .font(.title2)
-                    Spacer()
-                }
+            ZStack {
+                // background
+                AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .init(x: 0.825, y: 0.6))
+                    .ignoresSafeArea()
                 
-                HStack() {
-                    // user
-                    VStack {
-                        Text("User Hand")
-                            .font(.largeTitle.bold())
-                        
-                        HStack {
-                            ForEach(userHands, id:\.self) { hand in
-                                Button {
-                                    userChoosed = hand
-                                    handTapped()
-                                } label: {
-                                    Image(hand)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(maxWidth: 150, maxHeight: 150)
+                VStack {
+                    // score
+                    HStack {
+                        Text("Score: \(score)")
+                            .foregroundColor(.white)
+                            .font(.title2)
+                        Spacer()
+                    }
+                    
+                    // main body
+                    HStack() {
+                        // user
+                        VStack {
+                            Text("Choose Hand")
+                                .font(.largeTitle.bold())
+                                .foregroundColor(.white)
+                            
+                            HStack {
+                                ForEach(userHands, id:\.self) { hand in
+                                    Button {
+                                        userChoosed = hand
+                                        handTapped()
+                                    } label: {
+                                        HandImage(name: hand)
+                                    }
                                 }
                             }
                         }
-                    }
-                    .alert(winOrLose, isPresented: $isTapped) {
-                        Button("OK") {
-                            computerHandIndex = Int.random(in: 0...2)
-                            isHidden = true
-                            if userPlayed == maxPlay {
-                                isFinish = true
-                                winOrLose = "Finish"
+                        .alert(winOrLose, isPresented: $isTapped) {
+                            Button("OK") {
+                                computerHandIndex = Int.random(in: 0...2)
+                                isHidden = true
+                                if userPlayed == maxPlay {
+                                    isFinish = true
+                                    winOrLose = "Finish"
+                                }
+                            }
+                        } message: {
+                            Text("You choose: \(userChoosed.capitalized) \n Enemy Choose: \(userHands[computerHandIndex].capitalized)")
+                        }
+                        .alert(winOrLose, isPresented: $isFinish) {
+                            Button("Restart") {
+                                restart()
+                            }
+                        } message: {
+                            Text("Your Final Score is \(score)")
+                        }
+                        
+                        Spacer()
+                        
+                        // enemy
+                        VStack {
+                            Text("Computer")
+                                .font(.largeTitle.bold())
+                                .foregroundColor(.white)
+                            
+                            HStack {
+                                HandImage(name: computerHands[computerHandIndex])
+                                    .opacity(isHidden ? 0 : 1)
                             }
                         }
-                    } message: {
-                        Text("You choose: \(userChoosed.capitalized) \n Enemy Choose: \(userHands[computerHandIndex].capitalized)")
                     }
-                    .alert(winOrLose, isPresented: $isFinish) {
-                        Button("Restart") {
-                            restart()
-                        }
-                    } message: {
-                        Text("Your Final Score is \(score)")
-                    }
-                    
-                    Spacer()
-                    
-                    // enemy
-                    VStack {
-                        Text("Enemy")
-                            .font(.largeTitle.bold())
-                        
-                        HStack {
-                            Image(computerHands[computerHandIndex])
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: 150, maxHeight: 150)
-                                .opacity(isHidden ? 0 : 1)
-                        }
-                    }
+                    .padding()
                 }
-                .padding()
             }
         }
         
@@ -123,6 +128,16 @@ struct ContentView: View {
     }
 }
 
+struct HandImage: View {
+    var name: String
+    
+    var body: some View {
+        Image(name)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(maxWidth: 150, maxHeight: 150)
+    }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
