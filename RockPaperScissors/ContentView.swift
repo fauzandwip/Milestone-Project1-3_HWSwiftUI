82ef30cddec2ca41f03959c87c1d91f35719a379
fauzandp <fauzan27dp@gmail.com>
2023-06-02 20:23:31 +0700

    Milestone-Project1-3_HWSwiftUI | RockPapperScissors Game add logic after user tap button show alert to show win, lose, or draw add animation of computer hand

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -10,14 +10,18 @@ import SwiftUI
 struct ContentView: View {
     private var userHands = ["rock", "paper", "scissors"]
     private var computerHands = ["rock_right", "paper_right", "scissors_right"]
-    private var computerHand = Int.random(in: 0..<3)
+    @State private var computerHandIndex = Int.random(in: 0...2)
     
     @State private var userChoosed = "rock"
     
+    @State private var isTapped = false
+    @State private var winOrLose = ""
+    @State private var isHidden = true
+    
     var body: some View {
         NavigationStack {
-            HStack(spacing: 100) {
-                // user hand
+            HStack() {
+                // user
                 VStack {
                     Text("User Hand")
                         .font(.largeTitle.bold())
@@ -25,28 +29,40 @@ struct ContentView: View {
                     HStack {
                         ForEach(userHands, id:\.self) { hand in
                             Button {
+                                isHidden = false
+                                userChoosed = hand
+                                handTapped()
                             } label: {
                                 Image(hand)
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
-                                    .frame(maxWidth: 200, maxHeight: 200)
+                                    .frame(maxWidth: 150, maxHeight: 150)
                             }
                         }
                     }
                 }
+                .alert(winOrLose, isPresented: $isTapped) {
+                    Button("OK") {
+                        computerHandIndex = Int.random(in: 0...2)
+                        isHidden = true
+                    }
+                } message: {
+                    Text("You choose: \(userChoosed.capitalized) \n Enemy Choose: \(userHands[computerHandIndex].capitalized)")
+                }
                 
+                Spacer()
+                
+                // enemy
                 VStack {
                     Text("Enemy")
                         .font(.largeTitle.bold())
                     
-                    // computer hand
-                    Button {
-                        
-                    }label: {
-                        Image(computerHands[computerHand])
+                    HStack {
+                        Image(computerHands[computerHandIndex])
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 200, maxHeight: 200)
+                            .frame(maxWidth: 150, maxHeight: 150)
+                            .opacity(isHidden ? 0 : 1)
                     }
                 }
             }
@@ -54,7 +70,23 @@ struct ContentView: View {
         }
         
     }
+    
+    func handTapped() {
+        let computerChoosed = userHands[computerHandIndex]
+        
+        if userChoosed == "rock" && computerChoosed == "scissors" || userChoosed == "paper" && computerChoosed == "rock" || userChoosed == "scissors" && computerChoosed == "paper"{
+            winOrLose = "Win"
+        } else if userChoosed == "rock" && computerChoosed == "paper" || userChoosed == "paper" && computerChoosed == "scissors" || userChoosed == "scissors" && computerChoosed == "rock"{
+            winOrLose = "Lose"
+        } else {
+            winOrLose = "Draw"
+        }
+        
+        isTapped = true
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
